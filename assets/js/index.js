add9d95338607f5330cb1c4827c1446fd28011e3
Russell Frankpitt <rustyff123@gmail.com>
2024-03-14 22:01:34 +0100

    start button and reset added. js complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,85 @@
 import Board from "./board.js";
 
 let board = new Board(); // creates a new game board
+let remainingMoves = 20;
 
-// Examine the grid of the game board in the browser console.
-// Create the UI of the game using HTML elements based on this grid.
 console.log(board.grid);
 
-// Your code here
+const startGame = document.querySelector("#start-game");
+const restartGame = document.querySelector("#restart-game");
+const containerGrid = document.querySelector(".grid-container");
+const movesLeft = document.querySelector(".moves-left");
+
+// initialize game board
+const fillGameBoard = () => {
+    movesLeft.classList.remove("hidden");
+    containerGrid.style.gridTemplateColumns = `repeat(9, 1fr)`;
+    containerGrid.style.gridTemplateRows = `repeat(9, 1fr)`;
+    for (let i = 0; i < 9; i++) {
+        const row = i;
+        for (let j = 0; j < 9; j++) {
+            const col = j;
+
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridItem.setAttribute("data-row", row);
+            gridItem.setAttribute("data-col", col);
+            containerGrid.appendChild(gridItem);
+        }
+    }
+};
+
+// Check game state
+const checkGameState = () => {
+    if (remainingMoves === 0) {
+        movesLeft.innerText = "Game Over!";
+        // update ui for all grid items
+        const gridItems = document.querySelectorAll(".grid-item");
+        gridItems.forEach((item) => {
+            item.innerText = "";
+            item.classList.add("miss");
+        });
+        return false;
+    }
+    return true;
+};
+
+// Start game
+startGame.addEventListener("click", () => {
+    fillGameBoard();
+});
+
+// player clicks event handler
+containerGrid.addEventListener("click", (e) => {
+    // Get screen data coordinates
+    const row = e.target.getAttribute("data-row");
+    const col = e.target.getAttribute("data-col");
+    let gameState = checkGameState();
+    if (gameState) {
+        if (row === null || col === null) {
+            console.log("Nothing");
+        } else {
+            const currentDiv = e.target;
+            let possibleHit = board.makeHit(row, col);
+            if (possibleHit) {
+                currentDiv.classList.add("hit");
+                currentDiv.innerText = possibleHit;
+            } else {
+                currentDiv.classList.add("miss");
+                remainingMoves--;
+            }
+        }
+        movesLeft.innerText = `Moves Left: ${remainingMoves}`;
+    }
+
+    checkGameState();
+});
+// restart game
+restartGame.addEventListener("click", () => {
+    remainingMoves = 20;
+    containerGrid.innerHTML = "";
+    board = new Board();
+    // Refill the board
+    fillGameBoard();
+    console.log(board);
+});
